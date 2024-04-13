94a08aef7dcf0009b0561cd97f7148d3605ebb16
Hari <118122494+Hari0o@users.noreply.github.com>
2024-04-13 13:00:28 +0530

    Update portfoliodocker.sh

--- portfoliodocker.sh
+++ portfoliodocker.sh
@@ -2,7 +2,7 @@
 sudo mkdir haridockerapp
 cd haridockerapp
 sudo git clone https://github.com/Hari0o/DockerFiles.git .
-sudo docker build -t hari-dev-app -f portfoliodockerfile .
+sudo docker build -t hari-dev-app -f golddockerfile .
 sudo docker login --username haridev944 --password Saggar@123
-sudo docker tag hari-dev-app haridev944/hari-dev-app
-sudo docker push haridev944/hari-dev-app
+sudo docker tag hari-gold-app haridev944/hari-gold-app
+sudo docker push haridev944/hari-gold-app
