6596f9232986d6efe0491af8be6a612ba66b94a5
Hari <118122494+Hari0o@users.noreply.github.com>
2024-04-16 16:10:48 +0100

    Update kubernetes.sh

--- kubernetes.sh
+++ kubernetes.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #alias "kubectl=microk8s kubectl"
-microk8s kubectl create ns haridev
-mkdir haridevshop
-cd haridevshop
+microk8s kubectl create ns harigold
+mkdir harigoldshop
+cd harigoldshop
 git clone https://github.com/Hari0o/kubernetes.git .
 microk8s enable ingress
 microk8s kubectl apply -f deployment.yaml
