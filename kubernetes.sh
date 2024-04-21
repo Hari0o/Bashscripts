51a55e6e8239de92660bc720908d11d3ccfaf698
Hari <118122494+Hari0o@users.noreply.github.com>
2024-04-21 13:51:06 +0100

    Update kubernetes.sh

--- kubernetes.sh
+++ kubernetes.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #alias "kubectl=microk8s kubectl"
+microk8s kubectl delete ns harigold
 microk8s kubectl create ns harigold
+sudo rm -rf * harigoldshop
 mkdir harigoldshop
 cd harigoldshop
 git clone https://github.com/Hari0o/kubernetes.git .
