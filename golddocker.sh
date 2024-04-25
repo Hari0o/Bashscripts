1ca3788e7e0ec2686b2190665f9661937ad01fa1
Hari <118122494+Hari0o@users.noreply.github.com>
2024-04-25 17:20:16 +0100

    Update golddocker.sh

--- golddocker.sh
+++ golddocker.sh
@@ -2,8 +2,7 @@
 sudo rm -rf harigoldapp
 sudo mkdir harigoldapp
 cd harigoldapp
-sudo git clone https://github.com/Hari0o/DockerFiles.git .
+sudo git clone https://github.com/Hari0o/Gold_Site_Ecommerce.git .
 sudo docker build -t hari-gold-app -f golddockerfile .
-sudo docker login --username haridev944 --password Saggar@123
 sudo docker tag hari-gold-app haridev944/hari-gold-app
 sudo docker push haridev944/hari-gold-app
